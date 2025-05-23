86f1d79b7b01c5a47bd71b964a86580e1bc68a70
dibarra <da.ibarram@duocuc.cl>
2025-05-22 23:54:03 -0400

    Se crea Clase Store, getters y setters.

---
+++ src/main/java/cl/duoc/daibarram/ecomarketstoreservice/service/domain/Store.java
@@ -0,0 +1,57 @@
+package cl.duoc.daibarram.ecomarketstoreservice.service.domain;
+
+public class Store {
+    private int id;
+    private String name;
+    private String address;
+    private String phone;
+    private boolean active;
+
+    public Store(int id, String name, String address, String phone, boolean active) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.active = active;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
