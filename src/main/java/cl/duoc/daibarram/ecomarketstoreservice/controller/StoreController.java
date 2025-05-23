645e4211b957e91d6e367dd91c3c832408546ea6
dibarra <da.ibarram@duocuc.cl>
2025-05-23 01:08:17 -0400

    Se corrige ruta.

--- src/main/java/cl/duoc/daibarram/ecomarketstoreservice/controller/StoreController.java
+++ src/main/java/cl/duoc/daibarram/ecomarketstoreservice/controller/StoreController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/stores")
+@RequestMapping("/api/v1/stores")
 public class StoreController {
 
     @Autowired
