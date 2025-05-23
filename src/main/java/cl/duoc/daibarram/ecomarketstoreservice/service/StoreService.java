6db446c00448f323b3823fa259258282a1e8bd77
dibarra <da.ibarram@duocuc.cl>
2025-05-22 23:58:20 -0400

    Se corrige error en import de StoreService.

--- src/main/java/cl/duoc/daibarram/ecomarketstoreservice/service/StoreService.java
+++ src/main/java/cl/duoc/daibarram/ecomarketstoreservice/service/StoreService.java
@@ -2,11 +2,9 @@ package cl.duoc.daibarram.ecomarketstoreservice.service;
 
 import cl.duoc.daibarram.ecomarketstoreservice.repository.StoreRepository;
 import cl.duoc.daibarram.ecomarketstoreservice.repository.entity.StoreEntity;
-import org.apache.catalina.Store;
+import cl.duoc.daibarram.ecomarketstoreservice.service.domain.Store;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 import java.util.List;
