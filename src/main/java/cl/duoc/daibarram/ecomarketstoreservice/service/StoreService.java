cd86895e3f07a55cd857d2c17725670f8c298fed
dibarra <da.ibarram@duocuc.cl>
2025-05-22 23:54:07 -0400

    Error en StoreService. Revisar Clase Abstracta.

--- src/main/java/cl/duoc/daibarram/ecomarketstoreservice/service/StoreService.java
+++ src/main/java/cl/duoc/daibarram/ecomarketstoreservice/service/StoreService.java
@@ -1,11 +1,14 @@
 package cl.duoc.daibarram.ecomarketstoreservice.service;
 
 import cl.duoc.daibarram.ecomarketstoreservice.repository.StoreRepository;
+import cl.duoc.daibarram.ecomarketstoreservice.repository.entity.StoreEntity;
+import org.apache.catalina.Store;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,6 +17,20 @@ public class StoreService {
     @Autowired
     private StoreRepository storeRepository;
 
-
+    public List<Store> getStores() {
+        List<StoreEntity> stores = storeRepository.getAll();
+        List<Store> storeList = new ArrayList<>();
+        for (StoreEntity storeEntity : stores) {
+            storeList.add(
+                    new Store(
+                            storeEntity.getId(),
+                            storeEntity.getName(),
+                            storeEntity.getAddress(),
+                            storeEntity.getPhone(),
+                            storeEntity.isActive()
+                    ));
+        }
+        return storeList;
+    }
 
 }
